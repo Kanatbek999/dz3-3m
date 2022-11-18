e53da4f320a47476aaef4f35f6053322aa29958a
Kanatbek999 <kanatbekzamirbekov209>
2022-11-18 13:13:44 +0400

    dz-3-3m

---
+++ main.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('.menu')
+let row = document.querySelector('.row')
+
+const getProducts = (category) => {
+    fetch(`https://api.escuelajs.co/api/v1/products`)
+        .then((res) => res.json())
+        .then((res) => {
+            res.forEach((item) => {
+                menu.innerHTML += `
+                    <div class ="card">
+                    <img class = "card__img" src = "${item.category.image}" alt = ""/>
+                    <h2 class = "card__title">${item.title}</h2>
+                    <p class ="card__subtitle">${item.description}</p>
+                    <p class ="card__price">цена: ${item.price}</p>
+                 
+                `
+            })
+        })
+}
+
+getProducts()
+
+
